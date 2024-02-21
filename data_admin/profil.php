0665ad8921201c81e2094756d6194e2486e6619e
marselino kartawijaya <90839452+Acelino12@users.noreply.github.com>
2024-02-21 20:01:34 +0700

    add: view profil admin

---
+++ data_admin/profil.php
@@ -0,0 +1,62 @@
+
+<?php 
+    include "../db/koneksi.php";
+
+    session_start();
+
+    if (!isset($_SESSION['user'])){
+        header('location:login.php');
+    }
+
+    $username = $_SESSION['user']['username'];
+
+    $query = "SELECT * FROM tb_admin WHERE username='$username' ";
+    $sql = mysqli_query($koneksi,$query);
+    $result = mysqli_fetch_array($sql);
+    $email = $result['email'];
+    $password = $result['password'];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap 5 -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <!-- Vue -->
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <title>Profil Admin</title>
+</head>
+<body>
+<div id="app" style="width: 90%; background-color: #F6F3F3; border-radius: 10px; margin: 50px auto 0px auto; padding-bottom:10px; height:500px; box-shadow: 0px 0px 10px #2A2424; " >
+        <h2 style="text-align: center; padding-top: 15px; " >Orderan</h2>
+        <hr>
+        <label for="name">Username :</label>
+        <input type="text" readonly value="<?php echo $username; ?>" >
+        <br>
+        <label for="name">Email :</label>
+        <input type="text" readonly value="<?php echo $email; ?>" >
+        <br>
+        <label for="name">Password :</label>
+        <input type="text" readonly value="<?php echo $password; ?>" >
+    </div>
+
+    <script>
+        
+        const app = {
+            data() {
+                return {
+                    pass :false
+                }
+            },
+            methods: {
+                
+            },
+        }
+        Vue.createApp(app).mount('#app');
+    </script>
+
+</body>
+</html>
